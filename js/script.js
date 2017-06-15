894d277bdc6b5fa7198810ef0b1dd601f4120c3e
BerthaSchiebert <shirinova.valeria@yandex.ru>
2017-06-15 16:01:56 +0300

    bogdan footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 	var header = new Headhesive('.header');
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
 });
+
+function toggleCP() {
+	var cp = document.getElementById("cp");
+	cp.style.height = window.innerHeight - 0+"px";
+	if(cp.style.left == "0px"){
+		cp.style.left = "-220px";
+	} else {
+		cp.style.left = "0px";
+	}
+}
